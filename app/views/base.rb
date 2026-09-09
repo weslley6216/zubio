e83693862ca54480964eb04d89f4c1d2ce67e3f8
Weslley Campos <weslley.camphos@hotmail.com>
2026-09-09 16:14:10 -0300

    chore: drop the generator leftovers nothing in the app used
    hello_controller.js had no data-controller anywhere, en.yml no I18n caller,
    the CSP initializer no uncommented line, and jbuilder no JSON view in a
    Phlex-only app. ApplicationJob and Views::Base kept only commented-out
    generator hints.

--- app/views/base.rb
+++ app/views/base.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
 class Views::Base < Components::Base
-  # The `Views::Base` is an abstract class for all your views.
-
-  # By default, it inherits from `Components::Base`, but you
-  # can change that to `Phlex::HTML` if you want to keep views and
-  # components independent.
   include Phlex::Rails::Helpers::FormWith
   include Phlex::Rails::Helpers::Flash
 
-  # More caching options at https://www.phlex.fun/components/caching
   def cache_store = Rails.cache
 end
