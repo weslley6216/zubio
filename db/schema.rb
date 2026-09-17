29fab4ded3529c08b5c67005c79748835c52ee7c
Weslley Campos <weslley.camphos@hotmail.com>
2026-09-17 14:46:08 -0300

    feat: add onboarding_step to tenants and allow a nameless tenant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_10_120000) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_17_150000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -81,7 +81,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_120000) do
     t.datetime "created_at", null: false
     t.citext "custom_domain"
     t.datetime "custom_domain_verified_at"
-    t.string "name", null: false
+    t.string "name"
+    t.integer "onboarding_step", default: 0, null: false
     t.string "status", default: "active", null: false
     t.citext "subdomain", null: false
     t.datetime "updated_at", null: false
