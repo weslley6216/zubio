9de3fb6c30f0152405f74f29ddc9cbd2150ebee3
Weslley Campos <weslley.camphos@hotmail.com>
2026-09-17 14:50:08 -0300

    feat: let a professional replace its whole weekly schedule at once

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -6,4 +6,23 @@ class Professional < ApplicationRecord
 
   validates :display_name, presence: true
   validates :user_id, uniqueness: true, allow_nil: true
+
+  def replace_weekly_hours!(weekdays:, opens_at:, closes_at:, break_starts_at: nil, break_ends_at: nil)
+    transaction do
+      working_hours.destroy_all
+      weekdays.each do |weekday|
+        day_segments(opens_at, closes_at, break_starts_at, break_ends_at).each do |from, to|
+          working_hours.create!(weekday: weekday, opens_at: from, closes_at: to)
+        end
+      end
+    end
+  end
+
+  private
+
+  def day_segments(opens_at, closes_at, break_starts_at, break_ends_at)
+    return [ [ opens_at, closes_at ] ] if break_starts_at.blank? || break_ends_at.blank?
+
+    [ [ opens_at, break_starts_at ], [ break_ends_at, closes_at ] ]
+  end
 end
