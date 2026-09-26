94e7e19298e7872d488d8a1de6bbe9daae89caeb
Weslley Campos <weslley.camphos@hotmail.com>
2026-09-26 12:03:40 -0300

    feat: replace working hours per weekday through the aggregate root

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -7,14 +7,17 @@ class Professional < ApplicationRecord
   validates :display_name, presence: true
   validates :user_id, uniqueness: true, allow_nil: true
 
-  def replace_weekly_hours!(weekdays:, opens_at:, closes_at:, break_starts_at: nil, break_ends_at: nil)
+  def replace_working_hours!(declaration)
     transaction do
       working_hours.destroy_all
-      weekdays.each do |weekday|
-        WorkingHour.day_segments(opens_at, closes_at, break_starts_at, break_ends_at).each do |from, to|
-          working_hours.create!(weekday: weekday, opens_at: from, closes_at: to)
-        end
+      declaration.each do |weekday, ranges|
+        ranges.each { |opens_at, closes_at| working_hours.create!(weekday: weekday, opens_at: opens_at, closes_at: closes_at) }
       end
     end
+  end
+
+  def replace_weekly_hours!(weekdays:, opens_at:, closes_at:, break_starts_at: nil, break_ends_at: nil)
+    segments = WorkingHour.day_segments(opens_at, closes_at, break_starts_at, break_ends_at)
+    replace_working_hours!(weekdays.index_with { segments })
   end
 end
